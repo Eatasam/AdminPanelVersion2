5bbdd43d06b95054866a5361f0e1a0cf93942b6b
Eatasam <eatasams@gmail.com>
2025-07-06 17:19:06 +0300

    Fix reporter config for CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  // ✅ Correct
-  reporter: "mochawesome",
-
+  reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
     reportDir: "cypress/reports",
     overwrite: false,
     html: false,
     json: true,
   },
-
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
+      return config;
     },
   },
 });
