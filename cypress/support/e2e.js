3c7309ecbc15f68ed6ea361b0f2989d75d4a9ebc
Eatasam <eatasams@gmail.com>
2025-07-06 17:16:52 +0300

    Run Cypress with mochawesome reporter and update reports

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-mochawesome-reporter/register';
+
 Cypress.on('uncaught:exception', (err, runnable) => {
   // returning false prevents Cypress from failing the test
   return false;
